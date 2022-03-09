6d4a3cb4a7b11bc8e4cf09fc9389921e2f06e541
thikade <thikade>
2022-03-09 17:41:02 +0100

    switch repo to upstream for buildconfig

--- deployment/build-openshift.sh
+++ deployment/build-openshift.sh
@@ -1,5 +1,5 @@
 # use <git-repo-URL>#BRANCH to specify the Git repo
-$ oc new-build  https://github.com/thikade/kaffeehaus-demo#openshift --name kaffeehaus  --name kaffeehaus -o yaml --dry-run  | tee buildconfig.yaml
+$ oc new-build  https://github.com/sdaschner/kaffeehaus-demo#main --name kaffeehaus  --name kaffeehaus -o yaml --dry-run  | tee buildconfig.yaml
 
 $ oc apply -f buildconfig.yaml
 
